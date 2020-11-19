b2b827e85bf28949a39a6b291cb83cbea970db80
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-19 18:51:27 +0530

    [Tarun] Added : Feature to validate email id

--- user_registration/test_user_details.py
+++ user_registration/test_user_details.py
@@ -2,21 +2,37 @@ import pytest
 from user_registration.user_validation import UserRegistration
 
 
-@pytest.mark.parametrize('user_name', [("Yuvraj"), ("Dhoni"), ("Ganguly"), ("Sachin")])
-def test_for_valid_first_name(user_name):
-    assert UserRegistration.validate_first_name(user_name) == True
+@pytest.mark.parametrize('user_name, result',
+                         [("Yuvraj", True), ("Dhoni", True), ("Ganguly", True), ("Sachin", True), ("yuvraj", False),
+                          ("Dh", False), ("Ganguly12", False), ("Sachin?", False)])
+def test_for_validating_first_name(user_name, result):
+    assert UserRegistration.validate_first_name(user_name) == result
 
 
-@pytest.mark.parametrize('user_name', [("yuvraj"), ("Dh"), ("Ganguly12"), ("Sachin?")])
-def test_for_invalid_first_name(user_name):
-    assert UserRegistration.validate_first_name(user_name) == False
+@pytest.mark.parametrize('user_name, result',
+                         [("Yuvraj", True), ("Dhoni", True), ("Ganguly", True), ("Sachin", True), ("yuvraj", False),
+                          ("Dh", False), ("Ganguly12", False), ("Sachin?", False)])
+def test_for_validating_last_name(user_name, result):
+    assert UserRegistration.validate_last_name(user_name) == result
 
 
-@pytest.mark.parametrize('user_name', [("Yuvraj"), ("Dhoni"), ("Ganguly"), ("Sachin")])
-def test_for_valid_last_name(user_name):
-    assert UserRegistration.validate_last_name(user_name) == True
-
-
-@pytest.mark.parametrize('user_name', [("yuvraj"), ("Dh"), ("Ganguly12"), ("Sachin?")])
-def test_for_invalid_last_name(user_name):
-    assert UserRegistration.validate_last_name(user_name) == False
+@pytest.mark.parametrize('email_id, result',
+                         [("abc@yahoo.com", True), ("abc-100@yahoo.com", True), ("abc.100@yahoo.com", True),
+                          ("abc111@abc.com", True),
+                          ("abc-100@abc.net", True), ("abc.100@abc.com.au", True), ("abc@1.com", True),
+                          ("abc@gmail.com.com", True),
+                          ("abc+100@gmail.com", True), ("abc", False),
+                          ("abc@.com.my", False),
+                          ("abc123@gmail.a", False),
+                          ("abc123@.com", False),
+                          ("abc123@.com.com", False),
+                          (".abc@abc.com", False),
+                          ("abc()*@gmail.com", False),
+                          ("abc@%*.com", False),
+                          ("abc..2002@gmail.com", False),
+                          ("abc.@gmail.com", False),
+                          ("abc@abc@gmail.com", False),
+                          ("abc@gmail.com.1a", False),
+                          ("abc@gmail.com.aa.au", False)])
+def test_for_validating_email_id(email_id, result):
+    assert UserRegistration.validate_email_id(email_id) == result
--- user_registration/user_validation.py
+++ user_registration/user_validation.py
@@ -3,13 +3,22 @@ import re
 
 class UserRegistration:
     @staticmethod
-    def validate_first_name(first_name):
+    def validate_first_name(name):
         regex = '^[A-Z]{1}[A-Za-z]{2,}$'
-        if re.compile(regex).match(first_name):
+        return UserRegistration.check_regex(regex, name)
+
+    @staticmethod
+    def validate_last_name(name):
+        return UserRegistration.validate_first_name(name)
+
+    @staticmethod
+    def validate_email_id(email_id):
+        regex = '^[a-zA-Z]{3,}[0-9]{0,}([-._+]{1}[a-zA-Z0-9]{3,})?@[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{3}(.[a-zA-z]{2,4})?$'
+        return UserRegistration.check_regex(regex, email_id)
+
+    @staticmethod
+    def check_regex(regex, user_detail):
+        if re.compile(regex).match(user_detail):
             return True
         else:
             return False
-
-    @staticmethod
-    def validate_last_name(last_name):
-        return UserRegistration.validate_first_name(last_name)
