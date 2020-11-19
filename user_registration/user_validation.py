47472b04759a8a001a635c66fa02af9be2279682
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-19 18:10:56 +0530

    [Tarun] Refactored : Code to avoid DRY violation of validating user's first and last name

--- user_registration/user_validation.py
+++ user_registration/user_validation.py
@@ -12,8 +12,4 @@ class UserRegistration:
 
     @staticmethod
     def validate_last_name(last_name):
-        regex = '^[A-Z]{1}[A-Za-z]{2,}$'
-        if re.compile(regex).match(last_name):
-            return True
-        else:
-            return False
+        return UserRegistration.validate_first_name(last_name)
