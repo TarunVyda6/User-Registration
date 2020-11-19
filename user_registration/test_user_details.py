3cdf76b82cef3a6ec8a0364d80bfe9504b831261
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-19 18:04:42 +0530

    [Tarun] Added : Feature to validate user's last name

--- user_registration/test_user_details.py
+++ user_registration/test_user_details.py
@@ -10,3 +10,12 @@ def test_for_valid_first_name(user_name):
 @pytest.mark.parametrize('user_name', [("yuvraj"), ("Dh"), ("Ganguly12"), ("Sachin?")])
 def test_for_invalid_first_name(user_name):
     assert UserRegistration.validate_first_name(user_name) == False
+
+@pytest.mark.parametrize('user_name', [("Yuvraj"), ("Dhoni"), ("Ganguly"), ("Sachin")])
+def test_for_valid_last_name(user_name):
+    assert UserRegistration.validate_last_name(user_name) == True
+
+
+@pytest.mark.parametrize('user_name', [("yuvraj"), ("Dh"), ("Ganguly12"), ("Sachin?")])
+def test_for_invalid_last_name(user_name):
+    assert UserRegistration.validate_last_name(user_name) == False
--- user_registration/user_validation.py
+++ user_registration/user_validation.py
@@ -9,3 +9,11 @@ class UserRegistration:
             return True
         else:
             return False
+
+    @staticmethod
+    def validate_last_name(last_name):
+        regex = '^[A-Z]{1}[A-Za-z]{2,}$'
+        if re.compile(regex).match(last_name):
+            return True
+        else:
+            return False
